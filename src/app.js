807c8b2c03c3abbd12b7e8a41a5a22de7f21aa4c
16 Ocak Ders Sonu <mustafaoztosun.g@gmail.com>
2021-10-19 11:51:00 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Hello Kodlama.io")
+
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20 
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+
+const euroDun = 11.2
+//euroDun = 11 Bu hata fırlatıyor
+
+console.log(euroDun)
+//array = dizi demek
+//camelCasing = kelimelerin ilk harfi küçük
+//PascalCase = Kelimelerin ilk harfi büyük
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log ("<li>"+konutKredileri[i]+"</li>");
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+
